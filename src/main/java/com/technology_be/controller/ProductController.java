e078b66ece0cdb823283f9322f95310db6d125e7
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-06-11 10:44:51 +0700

    fix config

--- src/main/java/com/technology_be/controller/ProductController.java
+++ src/main/java/com/technology_be/controller/ProductController.java
@@ -1,45 +1,4 @@
 package com.technology_be.controller;
 
-import com.technology_be.model.Category;
-import com.technology_be.model.Product;
-import com.technology_be.service.CategoryService;
-import com.technology_be.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping(value = "/home")
 public class ProductController {
-
-    @Autowired
-    private ProductService productService;
-
-    @Autowired
-    private CategoryService categoryService;
-
-    /* Get all products */
-    @RequestMapping(value = "/products" , method = RequestMethod.GET)
-    public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
-        if(products.size() == 0) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(products , HttpStatus.OK);
-    }
-
-    /* Get all type products*/
-    @RequestMapping(value = "/category" , method = RequestMethod.GET)
-    public ResponseEntity<List<Category>> getCategories() {
-        List<Category> categories = categoryService.getCategories();
-        if(categories.size() == 0) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(categories , HttpStatus.OK);
-    }
 }
